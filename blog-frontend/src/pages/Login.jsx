75e676596ce8d9be2e0039820c8b5a5c19ff8ac3
Vala Dhruv <dkvala2003@gmail.com>
2024-03-29 15:04:42 +0530

    authentication complited

--- blog-frontend/src/pages/Login.jsx
+++ blog-frontend/src/pages/Login.jsx
@@ -1,11 +1,81 @@
 import React from 'react'
+import { Button, Card, CardBody, CardHeader, Col, Container, FormGroup, Input, Label, Row } from 'reactstrap'
+import { Form } from 'react-router-dom'
+import Base from '../component/Base'
 
 const Login = () => {
   return (
+    
     <div>
-      Login
-Login
+    <Base>
+
+    <Container >
+      <Row className='mt-3'>
+
+        <Col sm={{size:6,offset:3}}>
+
+          <Card color='dark' outline>
+
+           <CardHeader >
+              <h3>Fill All Details For Login...</h3>
+          </CardHeader>
+
+          <CardBody>
+
+            {/* creating form of signup page */}
+
+            <form>
+                  {/* email field */}
+                  <FormGroup>
+              
+                    <Label for="email">
+                      Enter Email
+                    </Label>
+                    <Input
+                      placeholder="Enter Email"
+                      type="email"
+                      id='email'
+                    />
+                  </FormGroup>
+
+                  {/* password field  */}
+                  <FormGroup>
+              
+                    <Label for="password">
+                      Enter Password
+                    </Label>
+                    <Input
+                      placeholder="Enter Password"
+                      type="password"
+                      id='password'
+                    />
+                  </FormGroup>
+
+              
+
+                  <Container className='text-center'>
+                      <Button color='dark' className='mt-2'>Register</Button>
+                      <Button color='secondary' type='reset' className='ms-2 mt-2'>Reset</Button>
+                  </Container>
+
+            </form>
+
+
+
+          </CardBody>
+          </Card>
+
+
+
+
+        </Col>
+    </Row>
+
+</Container>
+    </Base>
     </div>
+
+
   )
 }
 
--- blog-frontend/src/pages/SignUp.jsx
+++ blog-frontend/src/pages/SignUp.jsx
@@ -1,10 +1,102 @@
 import React from 'react'
+import Base from '../component/Base'
+import { Button, Card, CardBody, CardHeader, Col, Container, FormGroup, Input, Label, Row } from 'reactstrap'
+import { Form } from 'react-router-dom'
 
 const SignUp = () => {
   return (
     <div>
-      SignUp
-SignUp
+    <Base>
+
+    <Container>
+      <Row className='mt-3'>
+
+        <Col sm={{size:6,offset:3}}>
+
+          <Card color='dark' outline>
+
+           <CardHeader >
+              <h3>Fill All Details For Registration...</h3>
+          </CardHeader>
+
+          <CardBody>
+
+            {/* creating form of signup page */}
+
+            <form>
+                  {/* name field   */}
+                  <FormGroup>
+                    <Label for="name">
+                      Enter Name
+                    </Label>
+                    <Input
+                      
+                      placeholder="Enter Name"
+                      type="text"
+                      id='name'
+                    />
+                  </FormGroup>
+
+                  {/* email field */}
+                  <FormGroup>
+              
+                    <Label for="email">
+                      Enter Email
+                    </Label>
+                    <Input
+                      placeholder="Enter Email"
+                      type="email"
+                      id='email'
+                    />
+                  </FormGroup>
+
+                  {/* password field  */}
+                  <FormGroup>
+              
+                    <Label for="password">
+                      Enter Password
+                    </Label>
+                    <Input
+                      placeholder="Enter Password"
+                      type="password"
+                      id='password'
+                    />
+                  </FormGroup>
+
+                  {/* About field  */}
+                  <FormGroup>
+              
+                    <Label for="about">
+                      Enter About
+                    </Label>
+                    <Input
+                      placeholder="Enter about"
+                      type="textarea"
+                      id='about'
+                      style={{height:"10rem"}}
+                    />
+                  </FormGroup>
+
+                  <Container className='text-center'>
+                      <Button color='dark' className='mt-2'>Register</Button>
+                      <Button color='secondary' type='reset' className='ms-2 mt-2'>Reset</Button>
+                  </Container>
+
+            </form>
+
+
+
+          </CardBody>
+          </Card>
+
+
+
+
+        </Col>
+    </Row>
+
+</Container>
+    </Base>
     </div>
   )
 }
